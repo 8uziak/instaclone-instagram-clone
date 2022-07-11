05c599adcf436c2956313597beed1b41aa2408e6
Mateusz Buziak <mateusz.buziak@pfizer.com>
2022-07-11 20:31:26 +0200

    ver 0.21 trying to deploy app to aws eb via pipeline

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -129,3 +129,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AWS Elastic Beanstalk setup requirement
+ALLOWED_HOSTS = [
+    'http://djangoshop-env.eba-bkiqapqf.eu-central-1.elasticbeanstalk.com/',  
+]
